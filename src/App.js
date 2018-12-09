9adbfceb4aed6479f38f283293e53de47fc31261
tien1106 <buiminhtien06@gmail.com>
2018-12-09 20:21:24 +1100

    final template for React/Readux/Firebase/Router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component ,  Suspense, lazy} from 'react';
 
 
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import Home from './screens/Home/Home.js'
+import SignIn from './screens/SignIn/SignIn.js'
+import SignUp from './screens/SignUp/SignUp.js'
+import BuyAndSell from './screens/BuyAndSell/BuyAndSell.js'
+
 class App extends Component {
   render() {
     return (
       <div>
-
-     
       <Router>
-        <Route path='/' component = {Home}/>
+        <Switch> 
+          <Route exact path='/' component = {Home}/>
+          <Route path='/signin' component = {SignIn}/>
+          <Route path='/signup' component = {SignUp}/>
+          <Route path='/BuyAndSell' component = {BuyAndSell}/>
+        </Switch>
       </Router>
       </div>
     );
--- src/config/firebase/config.js
+++ src/config/firebase/config.js
@@ -1,1 +1,14 @@
-import 
+import firebase from 'firebase'
+import '@firebase/firestore' // 👈 If you're using firestore
+import ReduxSagaFirebase from 'redux-saga-firebase'
+require('dotenv').config();
+const myFirebaseApp = firebase.initializeApp({
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  databaseURL: process.env.databaseURL,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId
+})
+
+// const reduxSagaFirebase = new ReduxSagaFirebase(myFirebaseApp)
